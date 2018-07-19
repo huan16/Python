8bcc094f432ee480885338387d2a267c9905dd60
huan16 <38848138+huan16@users.noreply.github.com>
2018-07-19 17:33:03 +0800

    Initial commit

---
+++ MNIST_fashion_NN.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 19 17:28:58 2018
+
+@author: Administrator
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
